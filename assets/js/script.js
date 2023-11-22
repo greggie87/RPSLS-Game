5d8f711003710d8b153663f2f89d010b4af7b100
greggie87 <>
2023-11-22 19:33:31 +0000

    Add more game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ var num_games_count = 0;
 var userGuess;
 var computerGuess = function () { return computerChoices[Math.floor(Math.random() * computerChoices.length)]; };
 var computerReturn = function (play) {
-    var text;
+
     switch (play) {
         case "r":
             html_cpu_image.src = "assets/images/rock.png";
@@ -52,58 +52,43 @@ document.getElementById("rock").onclick = function () {
     userGuess = "r";
     html_user_image.src = "assets/images/rock.png";
     console.log(computerGuess());
-
     let play = computerGuess();
-
-
     return game(userGuess, play);
 };
 
 // USER PLAYS PAPER
-
 document.getElementById("paper").onclick = function () {
     userGuess = "p";
     html_user_image.src = "assets/images/paper.png";
     console.log(computerGuess());
-
     let play = computerGuess();
-
     return game(userGuess, play);
 };
 
 // USER PLAYS SCISSORS
-
 document.getElementById("scissors").onclick = function () {
     userGuess = "s";
     html_user_image.src = "assets/images/scissors.png";
     console.log(computerGuess());
-
     let play = computerGuess();
-
     return game(userGuess, play);
 };
 
 // USER PLAYS LIZZARD
-
 document.getElementById("lizard").onclick = function () {
     userGuess = "l";
     html_user_image.src = "assets/images/lizard.png";
     console.log(computerGuess());
-
     let play = computerGuess();
-
     return game(userGuess, play);
 };
 
 // USER PLAYS SPOCK
-
 document.getElementById("spock").onclick = function () {
     userGuess = "sp";
     html_user_image.src = "assets/images/spock.png";
     console.log(computerGuess());
-
     let play = computerGuess();
-
     return game(userGuess, play);
 };
 
@@ -113,41 +98,116 @@ let game = function (user, computer) {
 
     if (user == computer) {
         result.textContent = "Tie!";
-        result.style = "color: rgb(99, 189, 187)";
+        result.style = "color: rgb(245, 188, 74)";
+
     } else if (user == "r") {
         if (computer == "s") {
             userScore_count = userScore_count + 1;
             // userScore_count ++;
             result.textContent = "WIN";
-            result.style = "color: rgb(245, 188, 74)";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "l") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
             console.log(userScore_count);
         } else if (computer == "p") {
             computerScore_count = computerScore_count + 1;
             result.textContent = "LOSS";
             result.style = "color: rgb(243, 102, 82)";
+        } else if (computer == "sp") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
         }
+
     } else if (user == "p") {
         if (computer == "r") {
             userScore_count = userScore_count + 1;
             result.textContent = "WIN";
-            result.style = "color: rgb(245, 188, 74)";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "sp") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
             console.log(userScore_count);
         } else if (computer == "s") {
             computerScore_count = computerScore_count + 1;
             result.textContent = "LOSS";
             result.style = "color: rgb(243, 102, 82)";
+        } else if (computer == "l") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
         }
+
     } else if (user == "s") {
         if (computer == "p") {
-            userScore_count++;
+            userScore_count = userScore_count + 1;
             result.textContent = "WIN";
-            result.style = "color: rgb(245, 188, 74)";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "l") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "r") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
+        } else if (computer == "sp") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
+        }
+
+    } else if (user == "l") {
+        if (computer == "sp") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "p") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "s") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
         } else if (computer == "r") {
             computerScore_count = computerScore_count + 1;
             result.textContent = "LOSS";
             result.style = "color: rgb(243, 102, 82)";
         }
+
+    } else if (user == "sp") {
+        if (computer == "s") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "r") {
+            userScore_count = userScore_count + 1;
+            result.textContent = "WIN";
+            result.style = "color: rgb(99, 189, 187)";
+            console.log(userScore_count);
+        } else if (computer == "l") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
+        } else if (computer == "p") {
+            computerScore_count = computerScore_count + 1;
+            result.textContent = "LOSS";
+            result.style = "color: rgb(243, 102, 82)";
+        }
+
     }
+
     userScore.textContent = userScore_count;
     computerScore.textContent = computerScore_count;
 };
