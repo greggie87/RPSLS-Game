de447f7ed57584ebfaad806257ab96ff16eb8bdd
greggie87 <>
2023-11-22 15:57:07 +0000

    Add start of js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+// rules: 
+// - Scissors cut Paper
+// - Paper covers Rock
+// - Rock crushes Lizard
+// - Lizard poisons Spock
+// - Spock smashes Scissors
+// - Scissors decapitate Lizard
+// - Lizard eats Paper
+// - Paper disproves Spock
+// - Spock vaporizes Rock
+// - Rock crushes Scissors
+
+const userOptions = {
+  rock: "/images/rock.png",
+  paper: "/images/paper.png",
+  scissors: "/images/scissors.png",
+  lizard: "/images/lizard.png",
+  spock: "/images/Spock.png",
+};
+
 let playerScore = 0;
-let computerScore = 0;
-const pScore = document.getElementById('playerScore');
-const cScore = document.getElementById('computerScore');
-const compSelect = document.getElementById('computerSelect');
-const playerSelect = document.getElementById('playerSelect');
-const message = document.getElementById('message');
-let gameActive = false;
+let computerrScore = 0;
